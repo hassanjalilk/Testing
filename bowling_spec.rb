bbd25ac13fe318bbb145aaf261743b8367509939
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-06-16 10:24:02 -0400

    Testing

---
+++ bowling_spec.rb
@@ -0,0 +1,22 @@
+
+# require 'bowling'
+
+# describe Bowling, '#score' do
+# 	it 'returns zero for all th egutter game' do
+# 		bowling = Bowling.new
+# 		20.times { bowling.hit (0) }
+# 		bowling.score.should eq(0)
+# 	end
+# end
+
+
+class Bowling
+  def hit(pins)
+  end
+
+  def score
+    0
+  end
+end
+
+
